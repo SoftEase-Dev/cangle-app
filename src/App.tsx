2826a7972c9222d710dbb9e454f0d3c99040979d
Natabagass <meganatabagas@gmail.com>
2023-10-14 18:00:21 +0700

    style(slicing): slicing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='max-w-screen-2xl m-12'>
+      <div className='flex flex-col w-full'>
+        <div className='flex flex-row gap-5 w-full justify-between'>
+          <div className='flex flex-col w-full gap-5'>
+            <div className='border border-slate-600 rounded-lg h-full w-full'>
+
+            </div>
+
+            <div className='border border-slate-600 rounded-lg h-[50%] w-full'>
+              <div className='max-h-[300px] overflow-y-auto'>
+
+              </div>
+            </div>
+          </div>
+          <div className='w-[50%] flex flex-col gap-10'>
+            <div className='border border-slate-600 bg-slate-100 rounded-lg h-[250px] p-3'>
+
+            </div>
+
+            {/* Arrow Controller */}
+            <div className='w-full items-center flex flex-col'>
+              <div>
+                <span className='arrow up' />
+              </div>
+              <div className='flex flex-row justify-between w-[40%]'>
+                <span className='arrow left' />
+                <span className='arrow right' />
+              </div>
+              <div>
+                <span className='arrow down' />
+              </div>
+
+              <div className='w-full flex flex-col text-white'>
+                <button className='bg-black flex justify-center mt-5 rounded-lg p-3'>
+                  Capture Device
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
